64cefcf375188ad900b43b0088089dc65d147e0b
James West <james.west@westdarley.co.uk>
2018-01-31 08:52:08 +0000

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Content } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild(Content) content: Content;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  availableImages: string[] = [
+    'christmas-colour.png',
+    'christmas-dark.png'
+  ];
+
+
+  getFullPath(imgName: string){
+    return `assets/imgs/${imgName}`;
+  }
+
+  selectImage(imgName: string){
+    const scrollElement = this.content.getScrollElement();
+    scrollElement.style.backgroundImage = `url('${this.getFullPath(imgName)}')`;
+  }
+
+
+  ionViewDidLoad() {
+    window['abc'] = this.content;
+    console.log('doneeeee');
   }
 
 }
